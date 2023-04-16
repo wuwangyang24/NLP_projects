f8746ae683d9470c901da092968236334ed00071
Wu Wang Yang <56248896+wuwangyang24@users.noreply.github.com>
2023-04-16 07:45:32 +0200

    Update rl_criterion.py

--- fairseq_easy_extend/criterions/rl_criterion.py
+++ fairseq_easy_extend/criterions/rl_criterion.py
@@ -139,20 +139,8 @@ class RLCriterion(FairseqCriterion):
       repetition_ratio_sum = sum(log.get("repetition_ratio", 0) for log in logging_outputs)
 
       metrics.log_scalar(
-            "loss", loss_sum / sample_size / math.log(2), sample_size, round=3
+            "loss", loss_sum/len(logging_outputs)
         )
-
-      if sample_size != ntokens:
-            metrics.log_scalar(
-                "nll_loss", loss_sum / ntokens / math.log(2), ntokens, round=3
-            )
-            metrics.log_derived(
-                "ppl", lambda meters: utils.get_perplexity(meters["nll_loss"].avg)
-            )
-      else:
-          metrics.log_derived(
-              "ppl", lambda meters: utils.get_perplexity(meters["loss"].avg)
-          )
 
       metrics.log_scalar('repetition_ratio', repetition_ratio_sum/len(logging_outputs))
 
